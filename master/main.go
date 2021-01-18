a6fb7733f52f12fcf72c5eea23413582539891e7
Pham Duy <phd051199@gmail.com>
2021-01-18 23:15:32 +0700

    fix

--- master/main.go
+++ master/main.go
@@ -218,7 +218,7 @@ func main() {
 				Tty:             false,
 			}, &container.HostConfig{
 				Mounts: []mount.Mount{
-					mount.Mount{
+					{
 						Type:   mount.TypeBind,
 						Source: "/tmp/compiler/" + runningHash,
 						Target: "/workspace",
